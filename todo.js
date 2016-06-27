1e1ad6d46be79c3754d0232344572a0891ff6cdf
Marni Kostman <marnikostman@gmail.com>
2016-06-27 10:29:51 -0400

    Marni's first addition

---
+++ todo.js
@@ -0,0 +1,63 @@
+$("#hideToDo").click(function(){
+    $("#toDoField").hide();
+});
+
+$("#showToDo").click(function(){
+    $("#toDoField").show();
+});
+
+function get_todos() {
+    var todos = new Array;
+    var todos_str = localStorage.getItem('todo');
+    if (todos_str !== null) {
+        todos = JSON.parse(todos_str);
+    }
+    return todos;
+}
+
+function add() {
+    var task = document.getElementById('task').value;
+
+    var todos = get_todos();
+    if (task !== ""){
+      todos.push(task);
+    }
+    else {
+      alert("Invalid entry. No text entered.");
+    }
+    localStorage.setItem('todo', JSON.stringify(todos));
+
+    show();
+    return false;
+}
+
+function remove() {
+    var id = this.getAttribute('id');
+    var todos = get_todos();
+    todos.splice(id, 1);
+    localStorage.setItem('todo', JSON.stringify(todos));
+
+    show();
+
+    return false;
+}
+
+function show() {
+    var todos = get_todos();
+
+    var html = '<ul>';
+    for(var i=0; i<todos.length; i++) {
+        html += '<li>' + todos[i] + '<button class="remove" id="' + i  + '">x</button>' + '<button class="accomplished" id="' + i + '">&#10003;</button>' + '</li>';
+    };
+    html += '</ul>';
+
+    document.getElementById('todos').innerHTML = html;
+
+    var buttons = document.getElementsByClassName('remove');
+    for (var i=0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', remove);
+    };
+}
+
+document.getElementById('add').addEventListener('click', add);
+show();
