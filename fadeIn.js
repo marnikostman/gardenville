d9f780056f93c286fef8ce7d1d990fa60cb47937
Marc Salo <marcsalo455@gmail.com>
2016-07-01 09:51:26 -0400

    Changes to FAQ

--- fadeIn.js
+++ fadeIn.js
@@ -3,15 +3,3 @@ $("#nextday").click(function(){
     $("body").fadeTo(5000, 1);
   });
 });
-
-$('.master').hover(function(){
-  var title = $(this).attr('title');
-  $(this).data('tipText', title).removeAttr('title');
-  $('<p class="tooltip"></p>')
-  .text(title)
-  .appendTo('body')
-  .fadeIn('slow');
-}, function(){
-  $(this).attr('title', $(this).data('tipText'));
-  $('.tooltip').remove();
-});
