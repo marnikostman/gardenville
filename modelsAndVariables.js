4983663bdf5eb37467795881779ec107bf76d1de
Leslie Anderson <leslian.anderson@gmail.com>
2016-07-05 14:05:34 -0400

    split out models and next day functions

---
+++ modelsAndVariables.js
@@ -0,0 +1,56 @@
+var ticksPerMinute = 6000;
+var gameTick = 6000;
+var currency = {sunshine:10, energy:100, addEnergy: Date.now()}; //100 is the max energy
+var actionEnergyAmount = 10;
+var toDoListEnergy = 5;
+var journalSunshine = 20;
+var timedEnergy = 5;
+var energyMax = 100;
+var boardWidth = 7;
+var boardLength = 4;
+var teenPoints = 20;
+var adultPoints = 50;
+
+var graphicId = [
+  {name:"blank", state:[]},
+  {name:"rose", state:[]},
+  {name:"tulip", state:[]},
+  {name:"sunflower", state:[]},
+  {name:"house", state:[]},
+  {name: "alert", state:[]}];
+  //GraphicId[0].state[0] is the default which has nothing to layer at all.
+graphicId[0].state[0] = "<img src='./assets/blank.png'>";
+graphicId[1].state[0] = "<img src='./assets/stage_1_baby.png'>";
+graphicId[1].state[1] = "<img src='./assets/stage_2_teen.png'>";
+graphicId[1].state[2] = "<img src='./assets/stage_3_rose.png'>";
+graphicId[2].state[0] = "<img src='./assets/stage_1_baby.png'>";
+graphicId[2].state[1] = "<img src='./assets/stage_2_teen.png'>";
+graphicId[2].state[2] = "<img src='./assets/stage_3_tulip.png'>";
+graphicId[3].state[0] = "<img src='./assets/stage_1_baby.png'>";
+graphicId[3].state[1] = "<img src='./assets/stage_2_teen.png'>";
+graphicId[3].state[2] = "<img src='./assets/stage_3_sunflower.png'>";
+graphicId[4].state[0] = "<img src='./assets/house.png'>";
+graphicId[5].state[0] = "<img src='./assets/exclamation_alert.png'>";
+
+gameBoard = new Array(boardLength);
+for (var i = 0; i < boardLength; i++) //This is essentially the model
+{
+  gameBoard[i] = new Array(boardWidth);
+  for (var j = 0; j < boardWidth; j++)
+  {
+    gameBoard[i][j] = {
+      needTerraform: true,
+      type: "blank",
+      objectId: 0,
+      stateId: 0,
+      needFertilizer: true,
+      nextFertilizer: Date.now(),
+      harvest: false,
+      growthPoints: 0,
+      needWater: true,
+      nextWater: Date.now(),
+      effects: false,
+      usable: true
+    };
+  }
+}
---
+++ nextDayFunctions.js
@@ -0,0 +1,40 @@
+function updateGame(){
+  playTransition("rooster", "song1");
+  currency.energy = 100;
+  for(var i=0; i<boardLength; i++){
+    for(var j=0; j<boardWidth; j++){
+    setTimeout(nextDay, 1000, i, j);
+    }
+  }
+}
+
+function nextDay(x,y){
+  var maxstateId = 2;
+  var midstateId = 1;
+  var id = getId(x, y);
+  if (gameBoard[x][y].type != "blank" && gameBoard[x][y].type != "house")
+  {
+      gameBoard[x][y].needWater = true;
+      gameBoard[x][y].nextWater = Date.now();
+      gameBoard[x][y].needFertilizer=true;
+      gameBoard[x][y].nextFertilizer= Date.now();
+    if (gameBoard[x][y].stateId < midstateId && gameBoard[x][y].growthPoints >= teenPoints)
+    {
+      gameBoard[x][y].stateId=1;
+      drawCell(x, y, id);  
+    }
+    else if (gameBoard[x][y].stateId < maxstateId && gameBoard[x][y].growthPoints >= adultPoints){
+      gameBoard[x][y].stateId=2;
+      drawCell(x, y, id);  
+      if (gameBoard[x][y].stateId==2)
+      {
+          var rndNum = Math.floor((Math.random() * 10) + 1);
+          if (rndNum>6) //effects display only about 1/3 of the time
+          {
+            gameBoard[x][y].effects=true;
+            drawCell(x, y, id);   
+          }
+      }
+    }
+  }
+}
